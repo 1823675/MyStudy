4085e77bbc0960e239a2bd6be6a8b2f191631cd9
1823675 <a641881634@163.com>
2020-11-18 17:47:05 +0800

    init the project

---
+++ father/hello/haode.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "张守干"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+}
---
+++ father/hello/lianxi.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var w string = "我再写"
+	fmt.Println(w)
+
+	var b, c int = 3, 4
+	fmt.Println(b, c)
+}
